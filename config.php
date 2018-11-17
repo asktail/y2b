198b8cce08f75ee5373e3ccb9c190b8190d0993a
asktail <36602254+asktail@users.noreply.github.com>
2018-11-17 17:27:20 +0800

    Update config.php

--- config.php
+++ config.php
@@ -1,13 +1,13 @@
 <?php
 define('ROOT_PART', Root_part());
-define('APIKEY', 'AIzaSyCVkRcSiF0KmLCwmoo4edtw0vnSykCvFcc');
+define('APIKEY', 'AIzaSyDTus2uVnGOR5eC10YQbE3kpUmuvA6mGR8');
 define('GJ_CODE', 'US');
-define('SITE_NAME', 'y2b');
-define('TITLENAME', 'y2b');
+define('SITE_NAME', '自己看');
+define('TITLENAME', '自己看');
 define('EN2DEKEY', '32123wwerwer323tgfdgds');
-define('EMAIL', '1334347212@qq.com');
-define('NAME', 'admin');
-define('PASSWORD', 'admin');
+define('EMAIL', '1747415212@qq.com');
+define('NAME', 'asktail');
+define('PASSWORD', 'asktail');
 define('LOGINHTML', '<!DOCTYPE html>
 <html lang="en">
 <head>
